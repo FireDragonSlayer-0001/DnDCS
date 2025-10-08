69d8b2feba70da9cbb8780001d969952983ccf68
FireDragonSlayer <kennopolli@gmail.com>
2025-10-08 12:18:19 +0300

    Refactor UI state management and services

--- src/dndcs/ui/static/js/main.js
+++ src/dndcs/ui/static/js/main.js
@@ -1,4 +1,12 @@
 import { boot } from "./app.js";
+import * as api from "./services/api.js";
+import * as store from "./state/store.js";
+
+if (typeof window !== "undefined") {
+  window.DnDCS = window.DnDCS || {};
+  window.DnDCS.api = api;
+  window.DnDCS.store = store;
+}
 
 function start() {
   boot().catch((err) => {
---
+++ src/dndcs/ui/static/js/util/deepCopy.js
@@ -0,0 +1,3 @@
+export function deepCopy(value) {
+  return JSON.parse(JSON.stringify(value));
+}
